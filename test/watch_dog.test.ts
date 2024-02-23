351e8df677ee5618922875f7ef6c87c362399dfe
takoyaki-3 <mail@takoyaki3.com>
2024-02-23 13:32:52 +0900

    Initial commit

---
+++ test/watch_dog.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WatchDog from '../lib/watch_dog-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/watch_dog-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WatchDog.WatchDogStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
